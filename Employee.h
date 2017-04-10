23e03451b82a00ed3e97e2147d6ddc8ebf298ce4
chenchenpeng <pengy@william.jewell.edu>
2017-04-09 23:59:03 -0500

    Update Employee.h

--- Employee.h
+++ Employee.h
@@ -1,66 +1,56 @@
 #ifndef EMPLOYEE_H
 #define EMPLOYEE_H
 
-#include <stdio.h>
 #include <string>
-#include "Date.h"
-using namespace std;
 
 class Employee {
 
 private:
-	string name;
+
+	std::string name;
 	int waitTime;
 	int retainTime;
+
 public:
+
 	Employee() {
 		name = "unknown";
 		waitTime = 0;
 		retainTime = 0;
 	}
 
-	Employee(string newName) {
+	Employee(const std::string& newName) {
 		name = newName;
 		waitTime = 0;
 		retainTime = 0;
 	}
 
-	const Employee& operator = (const Employee& rhs) {
-		name = rhs.name;
-		waitTime = rhs.waitTime;
-		retainTime = rhs.retainTime;
-		return *this;
-	}
-
-	void setName(string newName) {
+	void setName(const std::string& newName) {
 		name = newName;
 	}
 
-	void wait(int wait) {
+	void addwait(int wait) {
 		waitTime += wait;
 	}
 
-	void retain(int retain) {
+	void addretain(int retain) {
 		retainTime += retain;
 	}
 
-	string getName() {
+	std::string getName() const {
 		return name;
 	}
 
-	int getWait() {
+	int getWait() const {
 		return waitTime;
 	}
 
-	int getRetain() {
+	int getRetain() const {
 		return retainTime;
 	}
 
-	friend bool operator<(const Employee& lhs, const Employee& rhs) {
-		return ((lhs.waitTime - lhs.retainTime) < (rhs.waitTime - rhs.retainTime));
-	}
-	friend bool operator>(const Employee& lhs, const Employee& rhs) {
-		return ((lhs.waitTime - lhs.retainTime) > (rhs.waitTime - rhs.retainTime));
+	int getPriority() const {
+		return waitTime - retainTime;
 	}
 };
 
